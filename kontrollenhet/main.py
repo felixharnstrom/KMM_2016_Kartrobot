3b1ae7596f3c0e587d69750ac42f6097c07cc059
Hannes Haglund <hanha265@student.liu.se>
2016-11-03 12:07:14 +0000

    Add test code, not tested.

--- kontrollenhet/main.py
+++ kontrollenhet/main.py
@@ -1,7 +1,4 @@
-import bluetooth
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
-print("found %d devices" % len(nearby_devices))
+import com.connect
 
-for addr, name in nearby_devices:
-    print("  %s - %s" % (addr, name))
+com.connect.debug_test()
