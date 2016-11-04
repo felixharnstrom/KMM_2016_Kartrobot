87c76b2df87d5c399385bf9400e7464ede0736cf
Felix Härnström <felha423@student.liu.se>
2016-11-04 17:23:46 +0100

    Implemented basic packet logic

--- dev/styrenhet/Styrenhet/Styrenhet/Styrenhet.c
+++ dev/styrenhet/Styrenhet/Styrenhet/Styrenhet.c
@@ -19,7 +19,7 @@ void uart_init (void)
 	UBRR0H = (BAUDRATE>>8);                      // shift the register right by 8 bits
 	UBRR0L = BAUDRATE;                           // set baud rate
 	UCSR0B|= (1<<TXEN0)|(1<<RXEN0);                // enable receiver and transmitter
-	UCSR0C|= (1<<USBS0)|(3<<UCSZ00);   // 8bit data format
+	UCSR0C|= (1<<USBS0)|(3<<UCSZ00);			// 8bit data format
 }
 
 // function to send data
@@ -30,28 +30,52 @@ void uart_transmit (unsigned char data)
 }
 
 // function to receive data
-unsigned char uart_recieve (void)
+unsigned char uart_receive (void)
 {
 	while(!(UCSR0A) & (1<<RXC0));                   // wait while data is being received
 	return UDR0;                                   // return 8-bit data
 }
 
+void receive_packet (char* buffer){
+	unsigned char a;
+	int i = 0;
+	while(i < 8){
+		a = uart_receive();
+		buffer[i] = a;
+		i++;
+	}
+}
+
 int main(void)
 {
-	unsigned char a;
-	char buffer[10];
-	DDRA = (1<<DDA7)|(1<<DDA6)|(1<<DDA5)|(1<<DDA4)|(1<<DDA3)|(1<<DDA2)|(1<<DDA1)|(1<<DDA0);	//All pins on port A as output
+	char c;
+	int packet[8];
+	//DDRA = (1<<DDA7)|(1<<DDA6)|(1<<DDA5)|(1<<DDA4)|(1<<DDA3)|(1<<DDA2)|(1<<DDA1)|(1<<DDA0);	//All pins on port A as output
 	uart_init();
     while(1)
     {
-		a=uart_recieve();
-		if(a == 'a'){
-			PORTA |= (1<<PORTA5);    //Turn 6th bit on PORTA (i.e. PA5) to 1 => on
-			//_delay_ms(1000);        //Delay for 1000ms => 1 sec
-		}else{
-			PORTA &= ~(1<<PORTA5);    //Turn 6th bit on PORTA (i.e. PA5) to 0 => off
-			//_delay_ms(1000);        //Delay for 1000ms => 1 sec
+		c = uart_receive();
+		if(c != 0x00){
+			for (int i = 0; i < 8; ++i) {
+				packet[i] = (c >> i) & 1;
+			}
+			for (int i = 7; i >= 0; --i) {
+				if(packet[i] == 1){
+					uart_transmit('1');
+					}else{
+					uart_transmit('0');
+				}
+			}
 		}
+		
+		
+		//if(a == 'a'){
+			//PORTA |= (1<<PORTA5);    //Turn 6th bit on PORTA (i.e. PA5) to 1 => on
+			////_delay_ms(1000);        //Delay for 1000ms => 1 sec
+		//}else{
+			//PORTA &= ~(1<<PORTA5);    //Turn 6th bit on PORTA (i.e. PA5) to 0 => off
+			////_delay_ms(1000);        //Delay for 1000ms => 1 sec
+		//}
 		//uart_transmit('a');
     }
 	
