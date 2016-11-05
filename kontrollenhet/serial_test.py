3af706c871f47d05ae93edc6705b6e70048b9beb
Silas Lenz <silas.lenz@gmail.com>
2016-11-05 14:25:39 +0100

    Add uart lib and test script

--- kontrollenhet/serial_test.py
+++ kontrollenhet/serial_test.py
@@ -1,5 +1,7 @@
-import serial
-ser = serial.Serial('/dev/ttyUSB0', 9600)  # open serial port
-while True:
-	print (ser.read())
-ser.close()             # close port
+from UART import UART
+uart = UART("ttyUSB0")
+print (uart.create_metapacket_hex(0,3,2))
+uart.send_packet(uart.create_metapacket(1,0,1))
+while 1:
+	print (uart.receive_packet())
+uart.close()
