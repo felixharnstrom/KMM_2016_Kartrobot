12239c903c017af1f6b02638ab56154752926d5c
Silas Lenz <silas.lenz@gmail.com>
2016-11-06 14:55:41 +0100

    Simplify sending of full instructions. Functions are created as classes.

--- kontrollenhet/modules.py
+++ kontrollenhet/modules.py
@@ -1,12 +1,30 @@
 from enum import Enum
 
-class Functions(Enum):
-	pass
 
-class StyrenhetFunctions(Functions):
-	SCAN = 0
-	DRIVE = 1
 
 class Adress(Enum):
 	STYRENHET = 0
 	SENSORENHET = 1
+
+
+class Function():
+	ADRESS = None
+	TYPE = None
+	LENGTH = None
+	ARGUMENTS = {}
+	LENGTH = len(ARGUMENTS)
+
+class StyrenhetFunction(Function):
+	ADRESS = 0
+
+class Scan(StyrenhetFunction):
+	TYPE = 0
+	ARGUMENTS = {"degrees" : 0,
+				"distance" : 0,
+				"speed" : 0}
+
+	def __init__(self, degrees : int, distance : int, speed : int):
+		self.ARGUMENTS["degrees"] = degrees
+		self.ARGUMENTS["distance"] = distance
+		self.ARGUMENTS["speed"] = speed
+		self.LENGTH = len(self.ARGUMENTS)
--- kontrollenhet/serial_test.py
+++ kontrollenhet/serial_test.py
@@ -2,8 +2,11 @@ from UART import UART
 from modules import *
 
 uart = UART("ttyUSB0")
-print (uart.create_metapacket_hex(Adress.SENSORENHET,0,StyrenhetFunctions.DRIVE))
-uart.send_packet(uart.create_metapacket(Adress.SENSORENHET,0,StyrenhetFunctions.DRIVE))
+scaninstruction = Scan(10,0,100)
+
+uart.send_function(scaninstruction)
+
 while 1:
 	print (uart.receive_packet())
+
 uart.close()
