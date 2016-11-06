e7a98b1c469a0cd64cb034667b62627c073b5f3d
Felix Härnström <felha423@student.liu.se>
2016-11-07 00:57:00 +0100

    Working test program

--- dev/kontrollenhet/robot_communication.py
+++ dev/kontrollenhet/robot_communication.py
@@ -6,9 +6,12 @@ s = server.server()
 s.start()
 s.connect()
 
-uart = UART("ttyUSB0")
+#uart = UART("ttyUSB0")
 while 1:
-	data_string = json.dumps({"uartData": uart.receive_packet().decode("utf-8")}) #data serialized
-	s.client.send(data_string.encode())
+	data = s.client.recv(1024).decode("utf-8") 
+	data_loaded = json.loads(data) #data loaded
+	#data_string = json.dumps({"uartData": uart.receive_packet().decode("utf-8")}) #data serialized
+	#s.client.send(data_string.encode())
+	print (data_loaded, type(data_loaded))
 
 s.close()
