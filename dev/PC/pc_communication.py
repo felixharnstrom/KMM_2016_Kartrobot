018cc991ed5aace14f6fca15256b53950446cf0b
Felix Härnström <felha423@student.liu.se>
2016-11-07 03:10:35 +0100

    Working example of sending instructions over WIFI

--- dev/PC/pc_communication.py
+++ dev/PC/pc_communication.py
@@ -2,12 +2,12 @@ import json
 import time
 from client import client
 robot = client()
-robot.start()
+robot.start(12345, "192.168.43.190")
 
 while 1:
-	data_string = json.dumps(["MOVE_SQUARES", {"DIRECTION" : 1, "SQUARES" : 2}]) #data serialized
+	data_string = json.dumps(["MOVE_MS", {"DIRECTION" : 1, "SPEED" : 100, "TIME" : 2000}]) #data serialized
 	robot.client.send(data_string.encode())
-	time.sleep(2)
+	time.sleep(5)
 	#data = robot.client.recv(1024).decode("utf-8") 
 	#data_loaded = json.loads(data) #data loaded
 	#print (data_loaded, type(data_loaded))
