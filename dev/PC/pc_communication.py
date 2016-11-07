c164018a93ad25863c9f1c190aadddb92372b7e4
Felix Härnström <felha423@student.liu.se>
2016-11-07 10:45:59 +0100

    Working example

--- dev/PC/pc_communication.py
+++ dev/PC/pc_communication.py
@@ -4,10 +4,20 @@ from client import client
 robot = client()
 robot.start(12345, "192.168.43.190")
 
-while 1:
-	data_string = json.dumps(["MOVE_MS", {"DIRECTION" : 1, "SPEED" : 100, "TIME" : 2000}]) #data serialized
-	robot.client.send(data_string.encode())
-	time.sleep(5)
+commands = [["MOVE_MS", {"DIRECTION" : 1, "SPEED" : 10, "TIME" : 1000}], ["MOVE_MS", {"DIRECTION" : 1, "SPEED" : 50, "TIME" : 2000}], ["MOVE_MS", {"DIRECTION" : 1, "SPEED" : 100, "TIME" : 3000}]]
+
+
+
+for command in commands:
+	data_string = json.dumps(command) #data serialized
+	robot.client.sendall("TEST".encode())
+	data = robot.client.recv(4096).decode("utf-8")
+	if (data == "ACK"):
+		print ("GOT ACK")
+		robot.client.sendall(data_string.encode())
+	time.sleep(0.01)
 	#data = robot.client.recv(1024).decode("utf-8") 
 	#data_loaded = json.loads(data) #data loaded
 	#print (data_loaded, type(data_loaded))
+
+robot.client.sendall("KILL".encode())
