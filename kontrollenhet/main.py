f816cf5649f06791e26c2fe70acafe1b7950eb74
Hannes Haglund <hanha265@student.liu.se>
2016-11-07 14:32:49 +0000

    Get message processing working.

--- kontrollenhet/main.py
+++ kontrollenhet/main.py
@@ -1,5 +1,6 @@
 
 # Hack to be able to import packages from parent directory
+# Basically fools main into believing it resides in ..
 # Only works in unix.
 # TODO: Temporary solution. Look into other solutions.
 import sys
@@ -12,10 +13,26 @@ if __name__ == '__main__' and __package__ is None:
 
 # Actual imports
 from .bluetooth.server import Server
+from process_request import process_request
+
 
 def main():
     server = Server()
-    print("Hello world")
+    print("Waiting for client connection.")
+    server.advertise_and_connect()
+    print("Connected succesfully")
+
+    # Main loop
+    while True:
+
+        #print(process_request(bytes(10)))
+        #continue
+    
+        # Process incoming messages
+        while server.messages_queued():
+            server.send(process_request(server.receive()))
+
+        # TODO: do other stuff
     
 
 if __name__ == "__main__":
