112105e806ba55e3943d3518b3f40c6205fb15d3
Jani Jokinen <Jani Jokinen>
2016-11-08 11:53:17 +0100

    Voltage to centimeters.

--- dev/Sensorenhet/Sensorenhet/Sensorenhet.c
+++ dev/Sensorenhet/Sensorenhet/Sensorenhet.c
@@ -4,11 +4,21 @@
  * Created: 11/4/2016 4:08:27 PM
  *  Author: emino969
  */ 
-#define F_CPU 8000000UL	//8MHz
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#ifndef PACKET_SIZE
+#define PACKET_SIZE 8   // define packet size
+#endif
+
+#include "UART.h"
 
 /* TODO should be a sub-program that reads all sensors and returns one int value per sensor
 void readSensor(int left1, int left2, int right1, int right2, int back)
@@ -57,7 +67,7 @@ void startConversion(uint8_t channel) {
 void initAD() {
     //ADEN: Enables ADC
     //ADPS[2:0]: Changes the clock divider 
-    ADCSRA |= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+    ADCSRA |= (1<<ADEN) | (0<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
     
     //REFS0: Selects AVCC as reference voltage (+5V)
     ADMUX |= (1<<REFS0);
@@ -72,6 +82,34 @@ double getVoltage() {
 }
 
 /*
+ * Convert voltage given by getVoltage() to distance in cm.
+ */
+double voltageToCentimeters(double voltage) {
+	// Formula approximated to the inverse of the following sets of data points (x, y):
+	/* 5 1.495
+	   7.5 1.420
+	   10 1.250
+	   15 0.845
+	   20 0.620
+	   25 0.500
+	   30 0.445 */
+	static const double A = 4.5770;
+	static const double B = -0.6340;
+	return pow(voltage / A, 1 / B);
+}
+
+/*
+ * Alternate way to calculate voltageToCentimeters.
+ * Does not work as well, currently
+ */
+double voltageToCentimetersLog(double voltage) {
+	static const double A = 2.6695;
+	static const double B = -0.6336;
+	return exp((voltage - A)/B);
+}
+
+
+/*
  *	Wait until there is no current conversions left
  */
 void waitForConversion() {
@@ -81,10 +119,11 @@ void waitForConversion() {
 int main(void) {
     DDRB |= (1<<DDB0);
     initAD();
+	uart_init();
 
     while(1) {
         //Loop over all channels
-        for (uint8_t i = 0; i < 4; i++) {
+        for (uint8_t i = 4; i < 5; i++) {
             startConversion(i);
             waitForConversion();
             
@@ -93,6 +132,21 @@ int main(void) {
             } else {
                 PORTB &= (0 << PORTB0);
             }
+			
+			int vint = voltageToCentimeters(getVoltage());
+			
+			//Get a string without any trash
+			int charCount = 32;
+			char vstr[charCount];
+			for (int i = 0; i < charCount; ++i)
+				vstr[i] = ' '; 
+			// int to str
+			sprintf(vstr, "%d", vint); 
+			// Send the string via UART
+			for (int i = 0; i < charCount; ++i)
+				if (vstr[i] != 0)
+					uart_transmit(vstr[i]);
+			uart_transmit('\n');
         }
     }
 }
