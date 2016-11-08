ca9e8ce072b06913e6004264786f00f8bfea8bba
Silas Lenz <silas.lenz@gmail.com>
2016-11-08 10:47:20 +0100

    Sends uart input data to PC

--- wifi/pc_communication.py
+++ wifi/pc_communication.py
@@ -1,10 +1,11 @@
 import json
-
 from client import client
 robot = client()
 robot.start()
 
-data = robot.client.recv(1024).decode("utf-8") 
-data_loaded = json.loads(data) #data loaded
+while 1:
+	
+	data = robot.client.recv(1024).decode("utf-8") 
+	data_loaded = json.loads(data) #data loaded
 
-print (data_loaded["test"])
+	print (data_loaded, type(data_loaded))
