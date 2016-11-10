555a1386fac47997d564c3d86793357e356cb3b1
Felix Härnström <felha423@student.liu.se>
2016-11-10 09:56:16 +0100

    Added simple test program

---
+++ dev/PC/pc_communication.py
@@ -0,0 +1,13 @@
+import json
+import time
+from client import client
+robot = client()
+robot.start()
+
+while 1:
+	data_string = json.dumps(["MOVE_SQUARES", {"DIRECTION" : 1, "SQUARES" : 2}]) #data serialized
+	robot.send(data_string.encode())
+	time.sleep(2)
+	#data = robot.client.recv(1024).decode("utf-8") 
+	#data_loaded = json.loads(data) #data loaded
+	#print (data_loaded, type(data_loaded))
