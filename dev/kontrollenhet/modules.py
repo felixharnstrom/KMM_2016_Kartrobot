f7d4cf7c5d2b5db55fa1e311fae83bb8c4765cfd
Daniel Månsson <danma344@student.liu.se>
2016-11-12 00:19:00 +0100

    Updated controller functions to be used with the new lists, also added a potential function to be used for getting controller data, with return parameters specified. It does however require server-side handling that does not currently exist.

--- dev/kontrollenhet/modules.py
+++ dev/kontrollenhet/modules.py
@@ -1,79 +1,88 @@
 from enum import Enum
 
-
-
 class Adress(Enum):
 	STYRENHET = 0
 	SENSORENHET = 1
 
-
+	
 class Function():
 	ADRESS = None
 	TYPE = None
-	LENGTH = None
-	ARGUMENTS = {}
-	LENGTH = len(ARGUMENTS)
+	LENGTH = 0
+	ARGUMENTS = []
 
+	
 class StyrenhetFunction(Function):
 	ADRESS = 0
 
 	
-
 class Drive(StyrenhetFunction):
 	TYPE = 1
 	ARGUMENTS = []
 
 	def __init__(self, direction : int, speed : int, time1 : int, time2 : int):
-		self.ARGUMENTS = []
+		self.ARGUMENTS.append(direction)
+		self.ARGUMENTS.append(speed)
+		self.ARGUMENTS.append(time1)
+		self.ARGUMENTS.append(time2)
+		self.LENGTH = len(self.ARGUMENTS)
+		
+		
+class Turn(StyrenhetFunction):
+	TYPE = 2
+	ARGUMENTS = []
+	#0 left, 1 right	
+	#0-100
+	#time = 2^8*time1 + time2 ms
+				
+	def __init__(self, direction : int, speed : int, time1 : int, time2 : int):
 		self.ARGUMENTS.append(direction)
 		self.ARGUMENTS.append(speed)
 		self.ARGUMENTS.append(time1)
 		self.ARGUMENTS.append(time2)
 		self.LENGTH = len(self.ARGUMENTS)
 
-		
-class Turn(StyrenhetFunction):
-	TYPE = 2
-	ARGUMENTS = {"direction" : 0, #0 left, 1 right
-				"speed" : 0, #0-100
-				"time1" : 0, #2^8*time1 + time2 ms
-				"time2" : 0}
-
-	def __init__(self, direction : int, speed : int, time1 : int, time2 : int):
-		self.ARGUMENTS["direction"] = direction
-		self.ARGUMENTS["speed"] = speed
-		self.ARGUMENTS["time1"] = time1
-		self.ARGUMENTS["time2"] = time2
-		self.LENGTH = len(self.ARGUMENTS)
-
 
 class SideSpeed(StyrenhetFunction):
 	TYPE = 3
-	ARGUMENTS = {"side" : 0, #0 left, 1 right
-				"direction" : 0, #0 backward, 1 forward
-				"speed" : 0 #0 - 100
-				}
+	ARGUMENTS = []
+	#0 left, 1 right
+	#0 backward, 1 forward
+	#0 - 100
 
 	def __init__(self, side : int, direction : int, speed : int):
-		self.ARGUMENTS["side"] = side
-		self.ARGUMENTS["direction"] = direction
-		self.ARGUMENTS["speed"] = speed
+		self.ARGUMENTS.append(side)
+		self.ARGUMENTS.append(direction)
+		self.ARGUMENTS.append(speed)
 		self.LENGTH = len(self.ARGUMENTS)
-
+	
 
 class Servo(StyrenhetFunction):
 	TYPE = 4
-	ARGUMENTS = {"degrees" : 0}
+	ARGUMENTS = []
 
 	enum 
 	def __init__(self, degrees : int):
-		self.ARGUMENTS["degrees"] = degrees #0 - 180
+		self.ARGUMENTS.append(degrees) #0 - 180
 		self.LENGTH = len(self.ARGUMENTS)
 		
 
 class StopMotors(StyrenhetFunction):
 	TYPE = 5
-	ARGUMENTS = {}
+	ARGUMENTS = []
 
 	def __init__(self):
 		self.LENGTH = 0
+		
+class ControllerInformation(StyrenhetFunction):
+	TYPE = 6
+	ARGUMENTS = []
+	#TODO: Implement pack receive method on python side
+	#Packet 0, 1 contains left side information
+	#Packet 2, 3 contains right side information
+	#0,2 contains motor direction, 0=backward, 1=forward
+	#1,3 contains motor PWM(M) (M/2.5 for speed percentage)
+	#4,5 contains servo PWM (S) 4 being MSB, 5 LSB eg S=[4]*2^8+[5] ((S-708)/8.45 for gyro angle)
+	
+	def __init__(self):
+		self.LENGTH = len()
