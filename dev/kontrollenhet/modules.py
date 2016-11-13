d26ce5b23a87d8b64538f61e85149871c656b3a1
Daniel Månsson <danma344@student.liu.se>
2016-11-13 13:53:36 +0100

    Added GetExecutableFunction in modules.py to get a runnable motor function from a function number and a dict with parameters. Also fixed some errors in the definitions of said motor functions. Beware that all functions are untested and might have problems with indentations due to spaces that (shouldn't) be a problem.

--- dev/kontrollenhet/modules.py
+++ dev/kontrollenhet/modules.py
@@ -1,21 +1,58 @@
 from enum import Enum
+import numpy as np
 
 class Adress(Enum):
 	STYRENHET = 0
 	SENSORENHET = 1
 
+class Functions(Enum):
+	DRIVE = 1
+	TURN = 2
+	SIDE_SPEED = 3
+	SERVO = 4
+	STOP_MOTORS = 5
+	CONTROLLER_INFORMATION = 6
 	
 class Function():
 	ADRESS = None
 	TYPE = None
 	LENGTH = 0
 	ARGUMENTS = []
-
 	
 class StyrenhetFunction(Function):
 	ADRESS = 0
 
+def splitTime(times): #Taken from robot_com
+	time_16 = np.uint16(times)
+	mask_high = 0xFF00
+	mask_low = 0x00FF
+	time_high = np.uint8((time_16 & mask_high) >> 8)
+	time_low = np.uint8((time_16 & mask_low))
+	return [time_high, time_low]
 	
+#Returns a StyrenhetFunction object for the correct integer (params = dict) 
+def GetExecutableFunction(function_number : int, params={}):
+	function_enum = Functions(function_number)    #Should work
+	#Params should be supplied correctly, otherwise we will have to handle it (TODO: implement check)
+	if(function_enum == Functions.DRIVE):
+		times = splitTime(params['TIME'])
+		return Drive(params['DIRECTION'], params['SPEED'], times[0], times[1])
+	elif(function_enum == Functions.TURN):
+		times = splitTime(params['TIME'])
+		return Turn(params['DIRECTION'], params['SPEED'], times[0], times[1])
+	elif(function_enum == Functions.SIDE_SPEED):
+		return SideSpeed(params['SIDE'], params['DIRECTION'], params['SPEED'])
+	elif(function_enum == Functions.SERVO):
+		return Servo(params['DEGREES'])
+	elif(function_enum == Functions.STOP_MOTORS):
+		return StopMotors()
+	elif(function_enum == Functions.CONTROLLER_INFORMATION):
+		return ControllerInformation()
+	else:
+		#Something has gone wrong, print that.
+		print("An incorrect function number has been given, number=",function_number)
+
+
 class Drive(StyrenhetFunction):
 	TYPE = 1
 	ARGUMENTS = []
@@ -35,8 +72,8 @@ class Turn(StyrenhetFunction):
 	#0-100
 	#time = 2^8*time1 + time2 ms
 				
-	def __init__(self, direction : int, speed : int, time1 : int, time2 : int):
-		self.ARGUMENTS.append(direction)
+	def __init__(self, turn : int, speed : int, time1 : int, time2 : int):
+		self.ARGUMENTS.append(turn)
 		self.ARGUMENTS.append(speed)
 		self.ARGUMENTS.append(time1)
 		self.ARGUMENTS.append(time2)
@@ -61,7 +98,6 @@ class Servo(StyrenhetFunction):
 	TYPE = 4
 	ARGUMENTS = []
 
-	enum 
 	def __init__(self, degrees : int):
 		self.ARGUMENTS.append(degrees) #0 - 180
 		self.LENGTH = len(self.ARGUMENTS)
@@ -85,4 +121,4 @@ class ControllerInformation(StyrenhetFunction):
 	#4,5 contains servo PWM (S) 4 being MSB, 5 LSB eg S=[4]*2^8+[5] ((S-708)/8.45 for gyro angle)
 	
 	def __init__(self):
-		self.LENGTH = len()
+		self.LENGTH = 0
