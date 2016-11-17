736b9c6eb40aac4c5e62448aeaacfdb12c75a8ea
Hannes Haglund <hanha265@student.liu.se>
2016-11-17 13:35:09 +0000

    Change names to reflect naming convention.

--- dev/wifi/communication.py
+++ dev/wifi/communication.py
@@ -2,20 +2,20 @@ from command import *
 import socket
 import json
 
-def constructMsg(command : Command):
+def construct_msg(command : Command):
     return json.dumps([command.address, command.command_type, command.arguments])
 
-def constructCommand(msg : list):
+def construct_command(msg : list):
     address = msg[0]
     msg_type = msg[1]
     args = msg[2]
-    return getExecutableCommand(msg_type + (address * 16), args)
+    return get_executable_command(msg_type + (address * 16), args)
 
-def transmitCommand(command : Command, socket : socket):
-    json_msg = constructMsg(command)
+def transmit_command(command : Command, socket : socket):
+    json_msg = construct_msg(command)
     socket.sendall(json_msg.encode())
 
-def receiveCommand(socket : socket):
+def receive_command(socket : socket):
     data = socket.recv(4096).decode("utf-8")
     json_msg = json.loads(data)
-    return constructCommand(json_msg)
+    return construct_command(json_msg)
