5f544a1e9fca839191ff5951100f464512b4ae26
Felix Härnström <felha423@student.liu.se>
2016-11-18 14:19:47 +0100

    Fixed derivative kick

--- dev/kontrollenhet/pid.py
+++ dev/kontrollenhet/pid.py
@@ -8,7 +8,7 @@ class Pid():
     output_data = 0
     setpoint = 0
     err_sum = 0
-    last_err = 0
+    last_input = 0
     kp = 0
     ki = 0
     kd = 0
@@ -21,13 +21,14 @@ class Pid():
         diff = time_now - self.time_last
         time_change = diff.microseconds / 1000              # Time diff in millis
 
+        # Error variables
         error = self.setpoint - self.input_data             # Proportional
         self.err_sum += (error * time_change)               # Integral
-        err_d = (error - self.last_err) / time_change       # Derivative
+        d_input = (self.input_data - self.last_input)
 
-        self.output_data = (self.kp * error) + (self.ki * self.err_sum) + (self.kd * err_d)
+        self.output_data = (self.kp * error) + (self.ki * self.err_sum) + (self.kd * d_input)
 
-        self.last_err = error
+        self.last_input = input_data
         self.time_last = time_now
 
     def set_tunings(self, p : float, i: float, d : float):
