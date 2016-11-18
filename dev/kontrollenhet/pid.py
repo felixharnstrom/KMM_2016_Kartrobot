e625187926c3fb3535b49b6ae5ef34689f6ed563
Felix Härnström <felha423@student.liu.se>
2016-11-18 15:13:19 +0100

    Bug fixes

--- dev/kontrollenhet/pid.py
+++ dev/kontrollenhet/pid.py
@@ -14,6 +14,8 @@ class Pid():
     kd = 0
     sample_time = 1000      # 1 sec
     automatic_mode = False
+    min_out = 0
+    max_out = 0
 
     MANUAL = 0
     AUTOMATIC = 1
@@ -22,66 +24,69 @@ class Pid():
         self.time_last = datetime.now()     # Need this to calculate the first time diff in compute()
 
     def compute(self):
-        if (!automatic_mode):
+        if (not self.automatic_mode):
             return
 
         time_now = datetime.now()
         diff = time_now - self.time_last
         time_change = diff.microseconds / 1000      # Time diff in millis
-        if (time_change >= sample_time):
+        if (time_change >= self.sample_time):
 
 
-        # Error variables
-        error = self.setpoint - self.input_data
-        self.i_term += (self.ki * error)            # Removes output bump when changing tuning parameters on the fly
+            # Error variables
+            error = self.setpoint - self.input_data
+            self.i_term += (self.ki * error)            # Removes output bump when changing tuning parameters on the fly
 
-        # Clamping the integration term to min/max values, to avoid this growing even when output is limited
-        if (self.i_term > max_out):
-            self.i_term = max_out
-        elif (self.i_term < min_out):
-            self.i_term = min_out
-        d_input = (self.input_data - self.last_input)
+            # Clamping the integration term to min/max values, to avoid this growing even when output is limited
+            if (self.i_term > self.max_out):
+                self.i_term = self.max_out
+            elif (self.i_term < self.min_out):
+                self.i_term = self.min_out
+            d_input = (self.input_data - self.last_input)
 
-        self.output_data = (self.kp * error) + self.i_term - (self.kd * d_input)
+            self.output_data = (self.kp * error) + self.i_term - (self.kd * d_input)
 
-        # Clamping output to min/max values
-        if (self.output_data > max_out):
-            self.output_data = max_out
-        elif (self.output_data < min_out):
-            self.output_data = min_out
+            # Clamping output to min/max values
+            if (self.output_data > self.max_out):
+                self.output_data = self.max_out
+            elif (self.output_data < self.min_out):
+                self.output_data = self.min_out
 
-        self.last_input = input_data
-        self.time_last = time_now
+            self.last_input = input_data
+            self.time_last = time_now
 
     def set_tunings(self, p : float, i: float, d : float):
-        sample_time_in_sec = sample_time / 1000
+        sample_time_in_sec = self.sample_time / 1000
         self.kp = p
         self.ki = i * sample_time_in_sec
         self.kd = d / sample_time_in_sec
 
     def set_sample_time(self, new_sample_time : int):
         ratio  = new_sample_time / self.sample_time
-        ki *= ratio
-        kd /= ratio
+        self.ki *= ratio
+        self.kd /= ratio
         self.sample_time = new_sample_time
 
     def set_output_limits(self, min_out : float, max_out : float):
 
+        self.min_out = min_out
+        self.max_out = max_out
+
         # Clamping output to min/max values
-        if (self.output_data > max_out):
-            self.output_data = max_out
-        elif (self.output_data < min_out):
-            self.output_data = min_out
+        if (self.output_data > self.max_out):
+            self.output_data = self.max_out
+        elif (self.output_data < self.min_out):
+            self.output_data = self.min_out
 
         # Clamping the integration term to min/max values, to avoid this growing even when output is limited
-        if (self.i_term > max_out):
-            self.i_term = max_out
-        elif (self.i_term < min_out):
-            self.i_term = min_out
+        if (self.i_term > self.max_out):
+            self.i_term = self.max_out
+        elif (self.i_term < self.min_out):
+            self.i_term = self.min_out
 
     def set_mode(self, mode : int):
         new_mode = (mode == self.AUTOMATIC)
-        if (new_mode && !self.automatic_mode):
+        if (new_mode and not self.automatic_mode):
             # From manual to auto
             self.initalize()
         self.automatic_mode = new_mode
@@ -91,7 +96,7 @@ class Pid():
         self.i_term = self.output_data
 
         # Clamping the integration term to min/max values, to avoid this growing even when output is limited
-        if (self.i_term > max_out):
-            self.i_term = max_out
-        elif (self.i_term < min_out):
-            self.i_term = min_out
+        if (self.i_term > self.max_out):
+            self.i_term = self.max_out
+        elif (self.i_term < self.min_out):
+            self.i_term = self.min_out
