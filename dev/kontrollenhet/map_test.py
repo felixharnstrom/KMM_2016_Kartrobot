f5f4bc59fd18d8efb8c849eb1b14719fbddaebde
Jani AndeoX Jokinen <Jani AndeoX Jokinen>
2016-11-20 00:42:35 +0100

    Added the posibility to add an angle for robot

--- dev/kontrollenhet/map_test.py
+++ dev/kontrollenhet/map_test.py
@@ -9,8 +9,6 @@ angle = 0
 
 plt.plot([x],[y], '.')
 
-plot_room([x,y,angle], plt, 'file')
-
-#plot_room([x, y, angle], plt, 'file', 'demo_data/triple_sided_wall_with_imperfections.json')
+plot_room([x,y,angle], plt)
 
 plt.show()
