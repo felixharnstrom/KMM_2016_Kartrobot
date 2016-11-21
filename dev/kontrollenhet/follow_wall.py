7e28811d2f39e64911fa8aa1ae864abd4724d607
Felix Härnström <felha423@student.liu.se>
2016-11-21 15:04:15 +0100

    Quick and dirty implementation of following walls around corners

--- dev/kontrollenhet/follow_wall.py
+++ dev/kontrollenhet/follow_wall.py
@@ -3,7 +3,11 @@ from command import Command
 from UART import UART
 import time
 import math
-import timeit
+
+#Define directions
+class Direction:
+    LEFT = 0
+    RIGHT = 1
 
 # Return a sensor value
 def read_sensor(sensor_instr : Command):
@@ -18,6 +22,32 @@ def read_sensor(sensor_instr : Command):
 
     uart_sensorenhet.close()
     return int.from_bytes(highest + lowest, byteorder="big", signed=True)
+
+def turn(direction : Direction, degrees : int):
+    #TODO: define general uart variables
+    uart_styrenhet = UART("tty.usbserial-FT94S3SE")
+
+    #Set the current direction to zero
+    current_dir = 0
+
+    #Set time to zero to turn untill stopped
+    turn_instr = Command.turn(1, 30, 0)
+    uart_styrenhet.send_command(turn_instr)
+
+    while (abs(current_dir) < degrees):
+        #Use a reimann sum to add up all the gyro rates
+        #Area = TimeDiff * turnRate 
+        clk = time.time()
+        turn_rate = read_sensor(Command.read_gyro()) / 100
+        current_dir += (time.time() - clk) * turn_rate
+    
+        print("Current dir: " + str(current_dir) + " -- Turn rate: " + str(turn_rate))
+        print("")
+
+    #Turning is completed, stop the motors
+    turn_instr = Command.stop_motors()
+    uart_styrenhet.send_command(turn_instr)
+    uart_styrenhet.close()
 
 #Take a average value from a given sensor
 def mean_sensor(it : int, sensor_instr : Command):
@@ -46,11 +76,33 @@ controller.set_sample_time(160)
 controller.set_output_limits(75,125)
 controller.set_mode(1)
 
-while 1:
-    # Get sensor values
-    ir_right_back = mean_sensor(5, Command.read_right_back_ir())
-    ir_right_front = mean_sensor(5, Command.read_right_front_ir())
+last_val = mean_sensor(5, Command.read_right_front_ir())
 
+while 1:
+    
+    # Get sensor values
+    ir_right_front = mean_sensor(5, Command.read_right_back_ir())
+    ir_right_back = mean_sensor(5, Command.read_right_front_ir())
+    
+    #print ("DIST: ", ir_right_front)
+    if (ir_right_front > (2 * last_val)):
+        print ("EDGE")
+        uart_styrenhet = UART("tty.usbserial-FT94S3SE")
+        stop = Command.stop_motors()
+        uart_styrenhet.send_command(stop)
+        time.sleep(1)
+        drive_instr = Command.drive(1, 50, 500)
+        #uart_styrenhet.send_command(drive_instr)
+        #time.sleep(1)
+        turn(Direction.RIGHT, 90)
+        time.sleep(1)
+        uart_styrenhet.send_command(drive_instr)
+        #time.sleep(1)
+        last_val = mean_sensor(5, Command.read_right_back_ir())
+        uart_styrenhet.close()
+        continue
+    last_val = ir_right_front
+    
     # We need to get the distance from the center of the robot perpendicular to the wall
     dist = (ir_right_front + ir_right_back) / 2
     angle = math.atan2(ir_right_back - ir_right_front, 95)  # 95 = distance between sensors
@@ -63,3 +115,4 @@ while 1:
     print ("OUTPUT: ", controller.output_data)
     drive(controller.output_data / 100, 50)
     print ("------")
+    
