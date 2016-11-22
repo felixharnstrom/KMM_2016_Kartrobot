7ba8e7fe9e8e1253744b6cc767d6a8ebf5fe7520
Jani Jokinen <janjo273@student.liu.se>
2016-11-22 01:59:29 +0100

    Added new funktionality to mapping, this time making tuples with line start and end coordinates for each line. Inside the test it can draw with this new information.

--- dev/kontrollenhet/map_test.py
+++ dev/kontrollenhet/map_test.py
@@ -1,5 +1,6 @@
 from map import *
 import matplotlib.pyplot as plt
+import numpy as np
 
 # robot position
 x = 0
@@ -10,6 +11,16 @@ angle = 0
 
 plt.plot([x],[y], '.')
 
-plot_room([x,y,angle], plt)
+map = []
 
+size = map_room([x,y,angle], map)
+
+for coord in map:
+    if coord[1] == coord[3]:
+        line = np.linspace(coord[0], coord[2], POINTS)
+        plt.plot(line, [coord[1]] * POINTS)
+    if coord[0] == coord[2]:
+        line = np.linspace(coord[1], coord[3], POINTS)
+        plt.plot([coord[0]] * POINTS, line)
+plt.plot([(size[0]-1)*GRID_SIZE, (size[1])*GRID_SIZE], [(size[2]-1)*GRID_SIZE, (size[3])*GRID_SIZE], '.')
 plt.show()
