adbb330964ecc6bd407030f69b628e66212ef98c
Jani Jokinen <janjo273@student.liu.se>
2016-11-23 01:27:24 +0100

    Added functionality to get all arches between possible grids/nodes

--- dev/kontrollenhet/map.py
+++ dev/kontrollenhet/map.py
@@ -5,6 +5,7 @@ import json
 import math, time
 import logging
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 
@@ -26,16 +27,12 @@ def map_room(robot, map):
     """
     This definition plots the room
     :param robot: A list with robot position and angle [x, y, angle]
-    :param plt: Pevious ploting of room
-    :return: Returns plt, that is the plot of room
+    :param map: Pevious mapping of the room
+    :return: Returns new_lines, that is a list of tupels with line information [(x11, y11, x21, y21), (x12, y12...), ...]
     """
     #coordinates = get_coordinates(measure_lidar(), robot)
 
     coordinates = get_coordinates(read_debug_data('demo_data/perfect_square_center_raw_data.json'), robot)
-    #coordinates = read_debug_data('demo_data/perfect_square_center.json')
-    #coordinates = read_debug_data('demo_data/triple_sided_wall_with_imperfections.json')
-    #coordinates = read_debug_data('demo_data/perfect_square_right_back_corner.json')
-    #coordinates = read_debug_data('demo_data/perfect_square_right_front_corner.json')
 
     # Gets size coordinate area in squares of GRID_SIZE
     # size = [min_x, max_x, min_y, max_y]
@@ -84,7 +81,10 @@ def map_room(robot, map):
                     map.append(line)
                     new_lines.append(line)
 
-    return size
+    # If you don't want it to plot all lines and all mesuring points then uncomment this
+    test_plot(map, coordinates, size)
+
+    return new_lines
 
 
 
@@ -98,18 +98,32 @@ def get_size(coordinates):
     max_x = 0
     min_y = 0
     max_y = 0
-
-    for x, y in coordinates:
-        if x < min_x:
-            min_x = x
-        elif x > max_x:
-            max_x = x
-        elif y < min_y:
-            min_y = y
-        elif y > max_y:
-            max_y = y
+    if len(coordinates[0]) == 2:
+        for x, y in coordinates:
+            check_size(x, min_x, max_x)
+            check_size(y, min_y, max_y)
+    elif len(coordinates[0]) == 4:
+        for x1, y1, x2, y2 in coordinates:
+            check_size(x1, min_x, max_x)
+            check_size(x2, min_x, max_x)
+            check_size(y1, min_y, max_y)
+            check_size(y2, min_y, max_y)
 
     return [int((min_x//GRID_SIZE)-1), int((max_x//GRID_SIZE)+2), int((min_y//GRID_SIZE)-1), int((max_y//GRID_SIZE)+2)]
+
+
+
+def check_size(val, min, max):
+    """
+    Checks if val is smaller than min value or larger than max value
+    :param val: Value to chek if it is smaller than min or larger than max
+    :param min: A min value to compare with and also the out value
+    :param max: A max value to compare with and also the out value
+    """
+    if val < min:
+        min = val
+    elif val > max:
+        max = val
 
 
 
@@ -187,6 +201,7 @@ def get_coordinates(mesurments, robot):
     return coordinates
 
 
+
 def read_debug_data(file_name):
     """
     Reads old measurements that have been saved to a json file
@@ -197,10 +212,11 @@ def read_debug_data(file_name):
     return data
 
 
+
 def measure_lidar():
     """
     Turns servo 180 degrees while scanning and converts the scanned points to cordinates.
-    :return: Measurements in [[x1,y2],[x2,y2], ...]
+    :return: Measurements in [[degree1, dist1],[degree2, dist2], ...]
     """
 
     uart = UART("COM4")
@@ -249,6 +265,57 @@ def measure_lidar():
     return measurements
 
 
+
+def check_available_grid(map):
+    size = get_size(map)
+    possible_squares = []
+
+    for y in range(size[2], size[3] - 1):
+        y_next = y + 1
+
+        for x in range(size[0], size[1] - 1):
+            x_next = x + 1
+
+            if ((x + 1) * GRID_SIZE, y * GRID_SIZE, (x + 1) * GRID_SIZE, y_next * GRID_SIZE) not in map:
+                possible_squares.append((x, y, x_next, y))
+            if (x * GRID_SIZE, (y + 1) * GRID_SIZE, x_next * GRID_SIZE, (y + 1) * GRID_SIZE) not in map:
+                possible_squares.append((x, y, x, y_next))
+
+    return possible_squares
+
+
+
+
+def plot_room(map):
+    """
+    Plots the room and return the plot
+    :param map: Is a list of tupels that has the information about walls [(x11, y11, x21, y21), (x12, y12, ...), ...]
+    :return: A list of coordinates
+    """
+    for coord in map:
+        if coord[1] == coord[3]:
+            line = np.linspace(coord[0], coord[2], POINTS)
+            plt.plot(line, [coord[1]] * POINTS)
+        if coord[0] == coord[2]:
+            line = np.linspace(coord[1], coord[3], POINTS)
+            plt.plot([coord[0]] * POINTS, line)
+    return plt
+
+
+
+def test_plot(map, coordinates, size):
+    """
+    A test plotting, that shows all mesuring points and walls on the same plot
+    """
+    plt = plot_room(map)
+    x_plot = []
+    y_plot = []
+    for x, y in coordinates:
+        x_plot.append(x)
+        y_plot.append(y)
+    plt.plot(x_plot, y_plot, '.')
+    plt.plot([(size[0] - 1) * GRID_SIZE, (size[1]) * GRID_SIZE], [(size[2] - 1) * GRID_SIZE, (size[3]) * GRID_SIZE], '.')
+    plt.show()
 
 
 
--- dev/kontrollenhet/map_test.py
+++ dev/kontrollenhet/map_test.py
@@ -1,6 +1,5 @@
 from map import *
 import matplotlib.pyplot as plt
-import numpy as np
 
 # robot position
 x = 0
@@ -9,18 +8,13 @@ y = 0
 # robot angle
 angle = 0
 
-plt.plot([x],[y], '.')
-
 map = []
 
-size = map_room([x,y,angle], map)
+map_room([x,y,angle], map)
 
-for coord in map:
-    if coord[1] == coord[3]:
-        line = np.linspace(coord[0], coord[2], POINTS)
-        plt.plot(line, [coord[1]] * POINTS)
-    if coord[0] == coord[2]:
-        line = np.linspace(coord[1], coord[3], POINTS)
-        plt.plot([coord[0]] * POINTS, line)
-plt.plot([(size[0]-1)*GRID_SIZE, (size[1])*GRID_SIZE], [(size[2]-1)*GRID_SIZE, (size[3])*GRID_SIZE], '.')
+print(map)
+print(check_available_grid(map))
+
+plt = plot_room(map)
+
 plt.show()
