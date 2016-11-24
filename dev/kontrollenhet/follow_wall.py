43901e2f75081677b225e089c5565148268fe46b
Felix Härnström <felha423@student.liu.se>
2016-11-24 13:39:04 +0100

    Wall following working (again)

--- dev/kontrollenhet/follow_wall.py
+++ dev/kontrollenhet/follow_wall.py
@@ -3,6 +3,7 @@ from command import Command
 from UART import UART
 import time
 import math
+import numpy as np
 
 #Define directions
 class Direction:
@@ -51,11 +52,11 @@ def turn(direction : Direction, degrees : int):
     
 
 #Take a average value from a given sensor
-def mean_sensor(it : int, sensor_instr : Command):
-    distance = 0
+def median_sensor(it : int, sensor_instr : Command):
+    distance = []
     for i in range(it):
-        distance += read_sensor(sensor_instr)
-    return distance / it
+        distance.append(read_sensor(sensor_instr))
+    return np.median(distance)
 
 def drive(ratio : int, base_speed : int):
     uart_styrenhet = UART("tty.usbserial-FT94S3SE")
@@ -65,12 +66,12 @@ def drive(ratio : int, base_speed : int):
     
     print("LEFT: ", round(left_speed))
     print("RIGHT: ", round(right_speed))
-    drive_instr = Command.side_speeds(1, round(left_speed), 1, round(right_speed))
+    drive_instr = Command.side_speeds(1, int(round(left_speed)), 1, int(round(right_speed)))
     uart_styrenhet.send_command(drive_instr)
 
 """
 uart_styrenhet = UART("tty.usbserial-FT94S3SE")
-turn_instr = Command.turn(1, 30, 1500)
+turn_instr = Command.turn(1, 30, 100)
 uart_styrenhet.send_command(turn_instr)
 uart_styrenhet.close()
 """
@@ -78,32 +79,38 @@ controller = Pid()
 controller.setpoint = 90
 controller.output_data = 0
 controller.set_tunings(0.7,0,0.6)
-controller.set_sample_time(160)
+controller.set_sample_time(110)
 controller.set_output_limits(-25,25)
 controller.set_mode(1)
 
-last_val = mean_sensor(5, Command.read_right_front_ir())
-#turn(Direction.LEFT, 90)
+last_val = read_sensor(Command.read_left_front_ir())
+#uart_styrenhet = UART("tty.usbserial-FT94S3SE")
+#drive_instr = Command.side_speeds(1, 50, 1, 50)
+#uart_styrenhet.send_command(drive_instr)
+
+while 0:
+    print(median_sensor(3, Command.read_right_front_ir()))
 
 while 1:
     
     # Get sensor values
-    ir_right_front = mean_sensor(5, Command.read_right_front_ir())
-    ir_right_back = mean_sensor(5, Command.read_right_back_ir())
+    ir_right_front = median_sensor(3, Command.read_right_back_ir())
+    ir_right_back = median_sensor(3, Command.read_right_front_ir())
     lidar = read_sensor(Command.read_lidar())
     #print ("DIST: ", ir_right_front)
-    
+    """
     if(lidar < 200):
         print ("OBSTACLE")
         uart_styrenhet = UART("tty.usbserial-FT94S3SE")
         stop = Command.stop_motors()
         uart_styrenhet.send_command(stop)
         time.sleep(1)
-        turn(Direction.LEFT, 90)
+        #turn(Direction.LEFT, 90)
         time.sleep(1)
         uart_styrenhet.close()
-        last_val = mean_sensor(5, Command.read_right_back_ir())
+        last_val = read_sensor(Command.read_right_back_ir())
         continue
+    """
     
     if (ir_right_front > (2 * last_val)):
         print ("EDGE")
@@ -111,14 +118,14 @@ while 1:
         stop = Command.stop_motors()
         uart_styrenhet.send_command(stop)
         time.sleep(1)
-        drive_instr = Command.drive(1, 50, 500)
-        #uart_styrenhet.send_command(drive_instr)
-        #time.sleep(1)
+        drive_instr = Command.drive(1, 30, 500)
+        uart_styrenhet.send_command(drive_instr)
+        time.sleep(1)
         turn(Direction.RIGHT, 90)
         time.sleep(1)
         uart_styrenhet.send_command(drive_instr)
-        #time.sleep(1)
-        last_val = mean_sensor(5, Command.read_right_back_ir())
+        time.sleep(1)
+        last_val = read_sensor(Command.read_right_back_ir())
         uart_styrenhet.close()
         continue
     last_val = ir_right_front
@@ -127,7 +134,8 @@ while 1:
     dist = (ir_right_front + ir_right_back) / 2
     angle = math.atan2(ir_right_back - ir_right_front, 95)  # 95 = distance between sensors
     perpendicular_dist = dist * math.cos(angle)
-
+    print ("FRONT: ", ir_right_front)
+    print ("BACK: ", ir_right_back)
     print ("DIST: ", dist)
     print ("PERPENDICULAR DIST: ", perpendicular_dist)
     controller.input_data = perpendicular_dist
@@ -136,4 +144,5 @@ while 1:
     print ("OUTPUT: ", controller.output_data)
     drive(controller.output_data / 100, 30)
     print ("------")
+    #time.sleep(0.1)
 
