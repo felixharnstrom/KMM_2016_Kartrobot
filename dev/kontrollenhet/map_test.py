2e3fe206330aaf1cb55622a940e6a0d5f56967d6
emino969 <emino969@student.liu.se>
2016-11-26 14:59:48 +0000

    Calibrate sleeps. Remove comment based version control and debug prints.

--- dev/kontrollenhet/map_test.py
+++ dev/kontrollenhet/map_test.py
@@ -2,8 +2,8 @@ from map import *
 import matplotlib.pyplot as plt
 
 # robot position
-x = -200
-y = -200
+x = 0
+y = 0
 
 # robot angle
 angle = 0
@@ -11,6 +11,7 @@ angle = 0
 map = []
 
 map_room(x,y,angle, map)
+print ("map",map)
 
 print(map)
 print(check_available_grid(map))
