d38d050bb74a1c04abe494bf28c1fff9a13d4cbe
Felix Härnström <felha423@student.liu.se>
2016-12-01 18:56:15 +0100

    Added verbosity level to command-line arguments.

--- dev/kontrollenhet/follow_wall.py
+++ dev/kontrollenhet/follow_wall.py
@@ -4,7 +4,8 @@ This program endlessly follows the wall on the robot's right side. The USB devic
 Examples:
         $ python3 follow_wall.py -s 'TTYUSB0' -c 'TTYUSB1'
 
-        $ python3 follow_wall.py -sensor 'TTYUSB0' -control 'TTYUSB1'
+        $ python3 follow_wall.py -sensor 'TTYUSB0' -control 'TTYUSB1' -v 3
+        
 Todo:
     * Automatically detect USB serial devices.
     * Use Daniel's enum for MANUAL, AUTONOM.
@@ -34,6 +35,7 @@ OBSTACLE_DIST = 200             # The maximum distance until an obstacle ahead w
 SENSOR_SPACING = 95             # Distance between side sensors on the robot.
 BASE_SPEED = 20                 # The speed that seems to work best for the controller with fully charged batteries.
 ACCELERATED_SPEED = 40          # Can be used when the controller is disengaged, otherwise too fast for the controller to handle.
+VEBOSITY = 1                    # Default verbosity level, can be set by command-line argument.
 
 # TODO: Use Daniel's enum for MANUAL, AUTONOM
 class ControllerMode:
@@ -154,8 +156,8 @@ class Robot:
             clk = time.time()
             turn_rate = self.read_sensor(Command.read_gyro()) / 100 - standstill_rate
             current_dir += (time.time() - clk) * turn_rate
-
-            #print("Current dir: " + str(current_dir) + " -- Turn rate: " + str(turn_rate))
+            if (VERBOSITY >= 2):
+                print("Current dir: " + str(current_dir) + " -- Turn rate: " + str(turn_rate))
 
         # Turning is completed, stop the motors
         turn_instr = Command.stop_motors()
@@ -194,8 +196,12 @@ class Robot:
         self.uart_styrenhet.send_command(Command.drive(1, speed, 0))
         lidar_init = self.read_sensor(Command.read_lidar())
         lidar_cur = lidar_init
+        if (VERBOSITY >= 2):
+            print("LIDAR INIT: ", lidar_init)
         while(lidar_init - lidar_cur < dist):
             lidar_cur = self.read_sensor(Command.read_lidar())
+            if (VERBOSITY >= 2):
+                print("LIDAR CURRENT: ", lidar_cur)
 
     def follow_wall(self, distance : int):
         """
@@ -233,7 +239,8 @@ class Robot:
             ir_left_front = self.median_sensor(IR_MEDIAN_ITERATIONS, Command.read_left_front_ir())
             ir_left_back = self.median_sensor(IR_MEDIAN_ITERATIONS, Command.read_left_back_ir())
             lidar = self.median_sensor(IR_MEDIAN_ITERATIONS, Command.read_lidar())
-            print ("IR_RIGHT_BACK: " + str(ir_right_back) + " IR_RIGHT_FRONT: " + str(ir_right_front) + " IR_LEFT_BACK: " + str(ir_left_back) + " IR_LEFT_FRONT: " + str(ir_left_front) + " LIDAR: " + str(lidar) + " GYRO: " + str(self._help_angle))
+            if (VERBOSITY >= 3):
+                print ("IR_RIGHT_BACK: " + str(ir_right_back) + " IR_RIGHT_FRONT: " + str(ir_right_front) + " IR_LEFT_BACK: " + str(ir_left_back) + " IR_LEFT_FRONT: " + str(ir_left_front) + " LIDAR: " + str(lidar) + " GYRO: " + str(self._help_angle))
 
              # Detect corridor to the right
             if ((ir_right_front >= TURN_OVERRIDE_DIST) or (ir_right_front > TURN_MIN_DIST and ir_right_front > EDGE_SPIKE_FACTOR * self._last_dist)):
@@ -261,7 +268,8 @@ class Robot:
             dist_left = (ir_left_front + ir_left_back) / 2
             angle_left = math.atan2(ir_left_back - ir_left_front, SENSOR_SPACING)
             perpendicular_dist_left = dist_left * math.cos(angle_left)
-            print ("PERPENDICULAR DIST RIGHT:", perpendicular_dist_right, "PERPENDICULAR DIST LEFT:", perpendicular_dist_left)
+            if (VERBOSITY >= 3):
+                print ("PERPENDICULAR DIST RIGHT:", perpendicular_dist_right, "PERPENDICULAR DIST LEFT:", perpendicular_dist_left)
             self.pid_controller.input_data = perpendicular_dist_right - perpendicular_dist_left
             self.pid_controller.compute()
             self.pid_controller.output_data += 100
@@ -355,7 +363,9 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--sensor", required = True, metavar = "SENSOR_DEVICE", help = "Device name of the USB<->serial converter for the sensor unit.")
     parser.add_argument("-c", "--control", required = True, metavar = "CONTROL_DEVICE", help = "Device name of the USB<->serial converter for the control unit.")
+    parser.add_argument("-v", "--verbosity", type = int, default = 1, choices=range(0, 4), help = "Verbosity level. 0 is lowest and 3 highest. Default 1.")
     args = parser.parse_args()
+    VERBOSITY = args.verbosity
     robot = Robot(ControllerMode.MANUAL, args.sensor, args.control)
 
     # Turn LIDAR to 90 degrees
@@ -370,23 +380,25 @@ def main(argv):
             # Drive forward without crashing in wall
             status = robot.follow_wall(9999999)
             if (status == DriveStatus.OBSTACLE_DETECTED):
+                if (VERBOSITY >= 1):
                     print ("---------- OBSTACLE DETECTED!")
                     print ("---------- TURNING LEFT 90 degrees")
-                    robot.turn(Direction.LEFT, 90, speed = ACCELERATED_SPEED)
-                    robot.pid_controller.kp = 0
-                    robot.pid_controller.ki = 0
-                    robot.pid_controller.kd = 0
-                    robot.pid_controller.initialize()
+                robot.turn(Direction.LEFT, 90, speed = ACCELERATED_SPEED)
+                robot.pid_controller.kp = 0
+                robot.pid_controller.ki = 0
+                robot.pid_controller.kd = 0
+                robot.pid_controller.initialize()
             elif (status == DriveStatus.RIGHT_CORRIDOR_DETECTED):
+                if (VERBOSITY >= 1):
                     print ("---------- DETECTED CORRIDOR TO RIGHT!")
                     print ("---------- TURNING RIGHT 90 degrees")
                     print (robot._help_angle)
-                    robot.turn(Direction.RIGHT, 90 + robot._help_angle, speed = ACCELERATED_SPEED)
-                    robot.drive_distance(RIGHT_TURN_EXIT_DIST, ACCELERATED_SPEED)
-                    robot.pid_controller.kp = 0
-                    robot.pid_controller.ki = 0
-                    robot.pid_controller.kd = 0
-                    robot.pid_controller.initialize()
+                robot.turn(Direction.RIGHT, 90 + robot._help_angle, speed = ACCELERATED_SPEED)
+                robot.drive_distance(RIGHT_TURN_EXIT_DIST, ACCELERATED_SPEED)
+                robot.pid_controller.kp = 0
+                robot.pid_controller.ki = 0
+                robot.pid_controller.kd = 0
+                robot.pid_controller.initialize()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
