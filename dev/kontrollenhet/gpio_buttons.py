e571607b6924c67944cb2c42cb0a3e24b2d7d2a1
Daniel Månsson <danma344@student.liu.se>
2016-12-12 19:07:46 +0100

    Cleaned up the thread initations. Changed some function names to better reflect what they do. Added polling for mode-switching as a separate thread

--- dev/kontrollenhet/gpio_buttons.py
+++ dev/kontrollenhet/gpio_buttons.py
@@ -1,46 +1,34 @@
-
 import RPi.GPIO as GPIO
 import sys
 import os
 import time
 import threading
+import mode
 
 SHUTDOWN_PIN = 2
 CHANGE_MODE_PIN = 14
 
-def init():
-    # tell the GPIO module that we want to use the
-    # chip's pin numbering scheme
-    GPIO.setmode(GPIO.BCM)
-    # Set pin 2 as in
-    GPIO.setup(SHUTDOWN_PIN, GPIO.IN)
-    GPIO.setup(CHANGE_MODE_PIN, GPIO.IN)
-
-def change_mode_is_pressed():
-    #return GPIO.input(SHUTDOWN_PIN)
-    return GPIO.input(CHANGE_MODE_PIN)
+def launch_poll_threads():
+    def init():
+        # tell the GPIO module that we want to use the
+        # chip's pin numbering scheme
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(SHUTDOWN_PIN, GPIO.IN)
+        GPIO.setup(CHANGE_MODE_PIN, GPIO.IN)
     
-def poll_shutdown():
-    if GPIO.input(SHUTDOWN_PIN):
-        # Turn pi off
-        os.system("shutdown now")
-
-def poll_shutdown_in_other_thread():
-    
-    def other_thread():
-        init()
+    def poll_shutdown():
         while True:
             time.sleep(0.05)
-            poll_shutdown()
-
-    t = threading.Thread(target=other_thread)
-    t.start()
-    return t
-
-
-
-# Test
-#poll_shutdown_in_other_thread()
-#time.sleep(600)
-
-
+            if GPIO.input(SHUTDOWN_PIN):
+                os.system("shutdown now")
+            
+    def poll_change_mode():
+        while True:
+            time.sleep(0.05)
+            if GPIO.input(CHANGE_MODE_PIN):
+                mode.toggle_mode()
+    init()
+    shutdown_t = threading.Thread(target=poll_shutdown)
+    shutdown_t.start()
+    change_mode_t = threading.Thread(target=poll_change_mode)
+    change_mode_t.start()
