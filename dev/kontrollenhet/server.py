a6e1302a102f32923acbc61252606e92fdb361af
Daniel Månsson <danma344@student.liu.se>
2016-12-12 19:16:29 +0100

    Added timeout for socket connection. Added handling to recover from socket errors, meaning that the socket will reconnect when the client attempts to, without crashing the program on the robot.

--- dev/kontrollenhet/server.py
+++ dev/kontrollenhet/server.py
@@ -9,11 +9,13 @@ class server():
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         self.socket.bind(("", port))    # Bind to the port
-
         self.socket.listen(5)           # Now wait for client connection.
 
     def connect(self):
-        self.client, addr = self.socket.accept() 
+        self.client, addr = self.socket.accept()
+        self.client.settimeout(3)
 
     def close(self):
-        self.client.close()
+        if self.client != None:
+            self.client.close()
+            self.client = None
