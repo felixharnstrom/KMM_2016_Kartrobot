dc7eeaf89fe1c3c163b3da580f8fd48ed009f65a
Hannes Haglund <hanha265@student.liu.se>
2016-12-14 18:02:43 +0100

    Fix misplaced origin.

--- dev/kontrollenhet/follow_wall.py
+++ dev/kontrollenhet/follow_wall.py
@@ -158,6 +158,7 @@ class Robot:
         uart_sensorenhet            (str): Device name of the USB<->Serial converter for the sensor unit.
         uart_styrenhet              (str): Device name of the USB<->Serial converter for the control unit.
         pid_controller              (Pid): The PID controller in use.
+        START_X                     (int): Starting x-value, in mm
         BLOCK_SIZE                  (int): Block size in millimetres.
         IR_MEDIAN_ITERATIONS        (int): Number of values to get from IR sensors.
         GYRO_MEDIAN_ITERATIONS      (int): Number of values to get from gyro.
@@ -183,6 +184,7 @@ class Robot:
         self.control_mode = mode
         self.path_trace = [] # (Angle, LengthDriven), with this list we can calculate our position
         self.path_queue = [] # (Blocks_To_Drive, Direction)
+        self.START_X = 200
         self.BLOCK_SIZE = 400
         self.IR_MEDIAN_ITERATIONS = 1
         self.GYRO_MEDIAN_ITERATIONS = 32
@@ -213,8 +215,8 @@ class Robot:
         init_UARTs()
 
         # Read start values for X and Y
-        self._x_start = self._median_sensor(5, Command.read_left_back_ir())+100
-        self._y_start = self._median_sensor(5, Command.read_lidar())+150
+        self._y_start = self._median_sensor(10, Command.read_right_back_ir())+100
+        self._x_start = self.START_X;
 
         
     def read_ir_side(self, side: Direction):
@@ -466,6 +468,9 @@ class Robot:
                 # Save the given length driven
                 self._save_position(reflex_right - reflex_right_start)
                 self._last_dist = self._median_sensor(self.IR_MEDIAN_ITERATIONS, Command.read_right_front_ir())
+                # Update map
+                self.update_map()
+
                 if side == "right":
                     return DriveStatus.CORRIDOR_DETECTED_RIGHT
                 else:
@@ -476,6 +481,8 @@ class Robot:
                 # Save the given length driven
                 handle_command(Command.stop_motors())
                 self._save_position(reflex_right - reflex_right_start)
+                # Update map
+                self.update_map()
                 return DriveStatus.OBSTACLE_DETECTED
 
             # We need to get the distance from the center of the robot perpendicular to the wall
@@ -498,6 +505,8 @@ class Robot:
         # Save the given length driven
         handle_command(Command.stop_motors())
         self._save_position(reflex_right - reflex_right_start)
+        # Update map
+        self.update_map()
         return DriveStatus.DONE
 
     # TODO: Add the correct code for updating the path_queue when the code is ready
@@ -725,7 +734,6 @@ def main(argv):
                 status = robot.follow_wall(9999999, side = "right")
             logger.info(robot.get_position())
             logger.info(robot.path_trace)
-            robot.update_map()
             
 
 
