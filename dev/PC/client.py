243c06bc5a3ea8a147ad6399124cc62d6ab5e0b5
Daniel Månsson <danma344@student.liu.se>
2016-12-19 12:29:58 +0100

    Full documentation for dev_pc. Removed old test file. Fixed client.py socket init.

--- dev/PC/client.py
+++ dev/PC/client.py
@@ -1,11 +1,29 @@
 import socket
 
 class client():
-    client = socket.socket()         # Create a socket object
-
+    """
+    Creates a simple socket which can be connected and closed.
+    
+    Attributes:
+        :attribute client (socket): The client socket.
+    """
+    def __init__(self):
+        self.client = socket.socket()
+    
     def start(self, port = 12345, ip = "localhost"):
+        """
+        Connects the client socket to the given ip and port number.
+        
+        Args:
+            :param port (int): The port number to use, defaults to 12345.
+            :param ip   (str): The ip-adress to use, defaults to 'localhost'.
+        """
+    
         port = 12345                # Reserve a port for your service.
         self.client.connect((ip, port))        # Bind to the port
 
     def close(self):
+        """
+        Closes the client socket connection.
+        """
         self.client.close()
