22005f231e8799510206ee430a7bb6f8b31fee28
Emil Norberg <emino969@student.liu.se>
2016-12-20 13:25:56 +0100

    robot_map_data.py is commented.

--- dev/kontrollenhet/robot_map_data.py
+++ dev/kontrollenhet/robot_map_data.py
@@ -1,12 +1,22 @@
 import threading
 import copy
 
+""" Lock for reading setting grid_map. """
 map_lock = threading.Lock()
+
+""" The global grid_map. """
 grid_map_output = [[0,0],[]]
+
+""" The driven distance for the robot. """
 robot_distance = 0
 
 def get_grid_map():
-	""" Reads the grid_map with a lock functionality and returns the grid_map. """
+	""" 
+	Reads the grid_map with a lock functionality and returns the grid_map. 
+	
+	Returns:
+		:return (2D list): A copy of the global grid_map.
+	"""
     global map_lock
     map_lock.acquire()
     gmap = copy.deepcopy(grid_map_output)
@@ -14,7 +24,12 @@ def get_grid_map():
     return gmap
 
 def set_grid_map(gmap):
-	""" Sets the grid_map with a lock functionality. """
+	""" 
+	Sets the grid_map with a lock functionality.
+	
+	Args:
+		:param gmap		(2D list): The new grid_map to update the global to.
+	"""
     global map_lock
     global grid_map_output
     map_lock.acquire()
